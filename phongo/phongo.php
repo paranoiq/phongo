112274943d9076fc6dbc542e64642d5f8ccf119d
Vlasta Neubauer <paranoiq@centrum.cz>
2010-08-18 00:47:29 +0200

    file shuffling

--- phongo/phongo.php
+++ phongo/phongo.php
@@ -16,9 +16,11 @@ if (!class_exists('Nette\Json', FALSE)) {
 require_once dirname(__FILE__) . '/libs/exceptions.php';
 require_once dirname(__FILE__) . '/libs/Phongo.php';
 require_once dirname(__FILE__) . '/libs/Object.php';
-require_once dirname(__FILE__) . '/libs/DateTime.php';
-require_once dirname(__FILE__) . '/libs/ObjectId.php';
-require_once dirname(__FILE__) . '/libs/Reference.php';
+
+require_once dirname(__FILE__) . '/libs/types/DateTime.php';
+require_once dirname(__FILE__) . '/libs/types/ObjectId.php';
+require_once dirname(__FILE__) . '/libs/types/Reference.php';
+
 require_once dirname(__FILE__) . '/libs/Tools.php';
 require_once dirname(__FILE__) . '/libs/Converter.php';
 require_once dirname(__FILE__) . '/libs/Connection.php';
@@ -29,7 +31,8 @@ require_once dirname(__FILE__) . '/libs/DatabaseInfo.php';
 require_once dirname(__FILE__) . '/libs/Cache.php';
 require_once dirname(__FILE__) . '/libs/Profiler.php';
 
-//require_once dirname(__FILE__) . '/libs/JsonSerialiser.php';
+require_once dirname(__FILE__) . '/libs/Json/Serialiser.php';
+require_once dirname(__FILE__) . '/libs/Json/Formater.php';
 
 function bar($value) {
     Nette\Debug::barDump($value);
