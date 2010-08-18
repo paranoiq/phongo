e9173c1ae6f0494dc0fe0c08576cdeb6a5ee6949
Vlasta Neubauer <paranoiq@centrum.cz>
2010-08-19 00:42:48 +0200

    types

---
+++ phongo/libs/Reference.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Phongo;
+
+use MongoDbRef;
+
+
+/**
+ * MongoDB object reference wrapper
+ * 
+ * @property-read id
+ * @property-read collection
+ * @property-read database
+ */
+class Reference extends Object {
+    
+    protected $id;
+    protected $collection;
+    protected $database;
+    
+    
+    /**
+     * @param string
+     * @param string
+     * @param string
+     */
+    public function __construct($id, $collection, $database = NULL) {
+        $this->id = $id;
+        $this->collection = $collection;
+        $this->database   = $database;
+    }
+    
+    /**
+     * @return array
+     */
+    public function getMongoDBRef() {
+        return MongoDBRef::create($this->collection, $this->id, $this->database);
+    }
+    
+    /**
+     * @param Phongo\Connection|Phongo\Database
+     * @return array
+     */
+    public function getObjectFrom($resource) {
+        return $resource->get($this);
+    }
+    
+    /**
+     * @return string
+     */
+    public function getDatabase() {
+        return $this->database;
+    }
+    
+    /**
+     * @return string
+     */   
+    public function getCollection() {
+        return $this->collection;
+    }
+    
+    /**
+     * @return string
+     */   
+    public function getId() {
+        return $this->id;
+    }
+    
+}
